39ba7f4ae625f118c47a33b00506329555caf49e
lyDonnieLiu <212616@qq.com>
2019-07-28 09:06:01 +0800

    ok

--- ImgPlusCharEmbedding.py
+++ ImgPlusCharEmbedding.py
@@ -10,6 +10,21 @@ import csv
 import numpy as np
 import pickle
 from sklearn.preprocessing import MinMaxScaler
+import sys
+import os
+from collections import namedtuple
+from keras_bert import load_vocabulary, load_trained_model_from_checkpoint, Tokenizer
+
+print('This demo demonstrates how to load the pre-trained model and extract word embeddings')
+
+def get_checkpoint_paths(model_path):
+    CheckpointPaths = namedtuple('CheckpointPaths', ['config', 'checkpoint', 'vocab'])
+    config_path = os.path.join(model_path, 'bert_config.json')
+    checkpoint_path = os.path.join(model_path, 'model.ckpt-0')
+    vocab_path = os.path.join(model_path, 'vocab.txt')
+    return CheckpointPaths(config_path, checkpoint_path, vocab_path)
+
+model_path = '../myBert/output'
 
 classes = 2
 maxlen = 50
@@ -17,8 +32,16 @@ train_file = '../myBert/dataset/train.tsv'
 test_file = '../myBert/dataset/test.tsv'
 AddImgFeature = True
 
+paths = get_checkpoint_paths(model_path)
 
-loaded_embedding = BertEmbedding(model="bert_12_768_12", dataset_name='wiki_cn', params_path='../myBert/bert_12_768_12_wiki_cn-885ebb9a.params', max_seq_length=maxlen)
+model = load_trained_model_from_checkpoint(paths.config, paths.checkpoint, seq_len=maxlen)
+model.summary(line_length=120)
+
+token_dict = load_vocabulary(paths.vocab)
+
+tokenizer = Tokenizer(token_dict)
+
+#loaded_embedding = BertEmbedding(model="bert_12_768_12", dataset_name='wiki_cn', params_path='../myBert/bert_12_768_12_wiki_cn-885ebb9a.params', max_seq_length=maxlen)
 
 def read_tsv(input_file, quotechar=None):
     """Reads a tab separated value file."""
@@ -46,19 +69,29 @@ def char_embedding(input_file, max_word=50, classes=2):
         sentences.append(line[0])
         y.append(line[1])
     #对于文档中的每个句子调用Bert的嵌入
-    result = loaded_embedding(sentences)
+    #result = loaded_embedding(sentences)
     #将每个句子嵌入截断或补足max_word个，不足的用最后一个embedding补足
-    for r in result:
-        if(AddImgFeature):
-            for i, c in enumerate(r[0]):
-                if c in char2id.keys():
-                    id = char2id[c] - 671               
-                    r[1][i] = np.sum([standard_img_feature[id], r[1][i]], axis=0)/2
+    # for r in result:
+    #     if(AddImgFeature):
+    #         for i, c in enumerate(r[0]):
+    #             if c in char2id.keys():
+    #                 id = char2id[c] - 671               
+    #                 r[1][i] = np.sum([standard_img_feature[id], r[1][i]], axis=0)/2
 
-        temp = r[1]
-        l = len(r[1])
-        temp = temp[:max_word] + [temp[-1]] * (max_word - l)
-        x.append(temp)
+    #     temp = r[1]
+    #     l = len(r[1])
+    #     temp = temp[:max_word] + [temp[-1]] * (max_word - l)
+    #     x.append(temp)
+    for s in sentences:
+        tokens = tokenizer.tokenize(s)
+        indices, segments = tokenizer.encode(first=s, max_len=maxlen)
+        predicts = model.predict([np.array([indices]), np.array([segments])])[0]
+        for i, token in enumerate(tokens):
+            if token in char2id.keys():
+                id = indices[i]-671
+                temp = np.sum(standard_img_feature[id],predicts[i])
+            x.append(temp)
+    
     y = keras.utils.to_categorical(y,num_classes=2)
     assert(len(x)==len(y))
     return np.array(x), np.array(y)
